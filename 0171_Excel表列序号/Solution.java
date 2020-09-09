c8308f37de15b5defb9a356c6221ac8936dab3d8
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-09-09 10:19:07 +0800

    Create Solution.java

---
+++ 0171_Excel表列序号/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+
+// ====================算法实现====================
+	// 1 26进制
+	// 时间复杂度O(n)
+	// 空间复杂度O(n)
+	public int titleToNumber(String s) {
+
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+
+		int result = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			result *= 26;
+			char temp = s.charAt(i);
+			result += temp - 'A' + 1;
+		}
+
+		return result;
+	}
+
+// ====================测试代码====================
+	public void test() {
+		String s = "ZY";
+		int result = titleToNumber(s);
+		System.out.println(result);
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		s.test();
+
+	}
+
+}
