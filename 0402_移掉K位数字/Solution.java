a14f9ad2ca8c8c39174427011b78556a64671118
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2021-02-17 15:58:27 +0800

    Create Solution.java

---
+++ 0402_移掉K位数字/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+
+// ===================算法实现=====================
+	public String removeKdigits(String num, int k) {
+
+		Deque<Character> deque = new LinkedList<Character>();
+		int length = num.length();
+		for (int i = 0; i < length; ++i) {
+			char c = num.charAt(i);
+
+			while (!deque.isEmpty() && k > 0 && deque.peekLast() > c) {
+				deque.removeLast();
+				k--;
+			}
+
+			deque.addLast(c);
+		}
+
+		for (int i = 0; i < k; ++i) {
+			deque.removeLast();
+		}
+
+		boolean lead = true;
+		StringBuilder sb = new StringBuilder();
+		while (!deque.isEmpty()) {
+
+			char c = deque.removeFirst();
+			if (lead && c == '0') {
+				continue;
+			}
+
+			lead = false;
+			sb.append(c);
+		}
+
+		return sb.length() == 0 ? "0" : sb.toString();
+	}
+
+// ===================测试代码=====================
+	public void test() {
+		String num = "1432219";
+		int k = 3;
+		String result = removeKdigits(num, k);
+		System.out.println(result);
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		s.test();
+
+	}
+
+}
